bd53968985ca6f6c61e25c839ebe7efef7d1b9a1
Ivan Dario Sandoval <nola2804@hotmail.com>
2016-07-19 14:19:43 -0500

    ad-hoc

---
+++ 458.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef vector < int > vi;
+typedef pair < int , int > ii;
+typedef pair < int , ii > iii;
+typedef vector < ii > vii;
+typedef long long ll;
+#define foi( i , k , n ) for( int i = k ; i < n ; i++ )
+#define size size()
+#define pb push_back
+#define openR freopen( "in" , "r" , stdin )
+#define openW freopen( "out" , "w" , stdout )
+#define impar( i ) i & 1
+#define par( i ) !( i & 1 )
+#define testBit( n , i ) ( n & ( 1 << i ) )
+#define turnOnBit( n , i ) ( n | ( 1 << i ) )
+#define ignore cin.ignore()
+
+int main()
+{
+  string linea;
+  while( getline( cin , linea ) )
+  {
+    foi( i , 0 , linea.size )
+      printf("%c", linea[i] - 7 );
+    printf("\n");
+  }
+  return 0;
+}
