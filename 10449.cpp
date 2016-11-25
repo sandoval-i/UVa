fc38ef2ceadc589f55842766b789229df0e1371f
Ivan Dario Sandoval <nola2804@hotmail.com>
2016-11-25 15:39:49 -0500

    not solved

--- 10449.cpp
+++ 10449.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-#define foi( i , n , k )    for( int i = n ; i < k ; ++i )
+#define foi( i , n , k )    for( int i = (int)n ; i < (int)k ; ++i )
 #define MAX_NUM_NODOS 205
 #define INF 1e8
 typedef pair < int , int > ii;
@@ -19,7 +19,7 @@ int busyness[MAX_NUM_NODOS];
 void dfs( int u , bitset < MAX_NUM_NODOS > &visited )
 {
     visited[u] = true;
-    foi( i , 0 , graph[u].size() )
+    for( int i = 0 ; i < graph[u].size() ; ++i )
     {
         int v = graph[u][i];
         if( !visited[v] )   dfs(v , visited);
@@ -28,22 +28,24 @@ void dfs( int u , bitset < MAX_NUM_NODOS > &visited )
 
 void bellman_ford()
 {
-    dist.assign( num_nodos , INF );
-    dist[0] = 0;
+    dist.assign( num_nodos + 1 , INF );
+    dist[1] = 0;
     cycle.reset();
-    foi( i , 0 , num_nodos - 1 )
-        foi( j , 0 , edges.size() )
+    for( int i = 0 ; i < num_nodos - 1 ; ++i )
+        for( int j = 0 ; j < edges.size() ; ++j )
         {
             int from = edges[j].first;
             int to = edges[j].second;
-            if( dist[from] + ( busyness[to] - busyness[from] ) < dist[to] )
-                dist[to] = dist[from] + ( busyness[to] - busyness[from] );
+            int cost = ( busyness[to] - busyness[from] ) * ( busyness[to] - busyness[from] ) * ( busyness[to] - busyness[from] );
+            if( dist[from] + cost < dist[to] )
+                dist[to] = dist[from] + cost;
         }
-    foi( i , 0 , edges.size() )
+    for( int i = 0 ; i < edges.size() ; ++i )
     {
         int from = edges[i].first;
         int to = edges[i].second;
-        if( dist[from] + ( busyness[to] - busyness[from] ) < dist[to] )
+        int cost = ( busyness[to] - busyness[from] ) * ( busyness[to] - busyness[from] ) * ( busyness[to] - busyness[from] );
+        if( dist[from] + cost < dist[to] )
         {
             cycle[to] = true;
             cycle[from] = true;
@@ -54,34 +56,34 @@ void bellman_ford()
 int main()
 {
     int num_edges , from , to , q , query , caso = 0;
-    while( scanf( "%d" , &num_nodos ) != EOF )
+    while( scanf( "%d" , &num_nodos ) == 1 )
     {
-        foi( i , 0 , num_nodos )    scanf( "%d" , &busyness[i] );
-        graph.assign( num_nodos , vector < int >() );
+        for( int i = 1 ; i <= num_nodos ; ++i )    scanf( "%d" , &busyness[i] );
+        graph.assign( num_nodos + 1 , vector < int >() );
         edges.clear();
         scanf( "%d" , &num_edges );
         while( num_edges-- )
         {
             scanf( "%d %d" , &from , &to );
-            edges.push_back( ii(--from , --to) );
+            edges.push_back( ii(from , to) );
             graph[from].push_back(to);
         }
         bellman_ford();
         visited_source.reset();
         negative_cycle.reset();
-        dfs( 0 , visited_source );
-        foi( i , 0 , num_nodos )
-            if( visited_source[i] && cycle[i] )
+        dfs( 1 , visited_source );
+        for( int i = 1 ; i <= num_nodos ; ++i )
+            if( visited_source[i] && cycle[i] && !negative_cycle[i] )
                     dfs( i , negative_cycle );
         scanf( "%d" , &q );
         printf( "Set #%d\n" , ++caso );
         while( q-- )
         {
             scanf( "%d" , &query );
-            --query;
-            if( dist[query] < 3 || negative_cycle[query] ) puts( "?" );
+            if( negative_cycle[query] || dist[query] == INF || dist[query] < 3 ) puts( "?" );
             else    printf( "%d\n" , dist[query] );
         }
     }
     return 0;
 }
+
