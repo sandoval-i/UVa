7853576a74a5df660a19a28c7374dd3efa09df88
Ivan Dario Sandoval <nola2804@hotmail.com>
2016-12-07 18:22:28 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,51 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import static sun.util.calendar.CalendarUtils.mod;
+
+
+public class Main
+{
+
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader sc = new BufferedReader( new InputStreamReader( System.in ) );
+        BigDecimal ans , aux;
+        int casos , fin;
+        casos = Integer.parseInt( sc.readLine() );
+        while(casos > 0)
+        {
+            casos--;
+            ans = BigDecimal.ZERO;
+            while(true)
+            {
+                aux = new BigDecimal(sc.readLine());
+                if( aux.equals(BigDecimal.ZERO) )   break;
+                ans = ans.add(aux);
+            }
+            if( ans.equals(BigDecimal.ZERO) )
+                System.out.println("0");
+            else 
+            {
+                String res = ans.toPlainString();
+                fin = res.length();
+                for( int i = res.length() - 1 ; i >= 0 ; --i )
+                {
+                    if( res.charAt(i) != '0' )
+                    {
+                        if( res.charAt(i) == '.' )
+                            fin = i;
+                         break;
+                    }
+                    fin = i;
+                }
+                for( int i = 0 ; i < fin ; ++i )
+                    System.out.print(res.charAt(i));
+                System.out.println();
+            }
+        }
+    }
+    
+}
