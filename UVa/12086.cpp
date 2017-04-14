99e9b9505e4737750b61dee138d468ee5efdc6a1
Ivan Dario Sandoval <nola2804@hotmail.com>
2017-04-14 15:22:03 -0500

    arreglos varios

--- UVa/12086.cpp
+++ UVa/12086.cpp
@@ -2,94 +2,88 @@
 
 using namespace std;
 
-typedef vector < int > vi;
-typedef pair < int , int > ii;
-typedef pair < int , ii > iii;
-typedef vector < ii > vii;
-typedef long long ll;
-#define foi( i , k , n ) for( int i = k ; i < n ; ++i )
-#define size size()
-#define pb push_back
-#define impar( i ) i & 1
-#define par( i ) !( i & 1 )
+#define foi(i,k,n)  for(int i = (int)k; i < (int)n; ++i)
 
-struct SegmentTree
-{
+struct st {
+  vector < int > tree;
+  int tam;
 
-  vi tree , lazyPropagation;
-
-  int armar( int low , int high , int pos , vi &array )
-  {
-    if( low == high )
-      return tree[pos] = array[low];
-    if( low > high )  return 0;
-    int mid = ( ( low + high ) >> 1 );
-    return tree[pos] = armar( low , mid , ( pos << 1 ) + 1 , array ) + armar( mid + 1 , high , ( pos << 1 ) + 2 , array );
+  st(vector < int >& arr) {
+    tree.resize(arr.size() << 2);
+    tam = arr.size();
+    build(0, tam - 1, 0, arr);
   }
 
-  SegmentTree( vi &array )
-  {
-    tree.assign( 4 * array.size , 0 );
-    armar( 0 , array.size - 1 , 0 , array );
-  }
-
-  void update( int &index , int low , int high , int pos , int &cantidad )
-  {
-    if( index < low || index > high )
-      return;
-    if( low == high )
-    {
-      tree[pos] = cantidad;
+  void build(int low, int high, int pos, vector < int >& arr) {
+    if(low == high) {
+      tree[pos] = arr[low];
       return;
     }
-    int mid = ( ( low + high ) >> 1 );
-    update( index , low , mid , ( pos << 1 ) + 1 , cantidad );
-    update( index , mid + 1 , high , ( pos << 1 ) + 2 , cantidad );
-    tree[pos] = tree[ ( pos << 1 ) + 1 ] + tree[ ( pos << 1 ) + 2 ];
+    int mid = (low + high) >> 1;
+    int left = (pos << 1) | 1;
+    int right = 1 + left;
+    build(low, mid, left, arr);
+    build(1 + mid, high, right, arr);
+    tree[pos] = tree[left] + tree[right];
   }
 
-  int sumRange( int qlow , int qhigh , int low , int high , int pos )
-  {
-    if( qlow > high || qhigh < low ) return 0; // No overlap
-    if( qlow <= low && qhigh >= high )  return tree[pos]; // Complete overlap
-    int mid = ( ( low + high ) >> 1 ); // Partial overlap
-    return sumRange( qlow , qhigh , low , mid , ( pos << 1 ) + 1 ) + sumRange( qlow , qhigh , mid + 1 , high , ( pos << 1 ) + 2 );
+  int query(int& qlow, int& qhigh, int low, int high, int pos) {
+    if(qhigh < low || qlow > high)  return 0;
+    if(low >= qlow && high <= qhigh)  return tree[pos];
+    int mid = (low + high) >> 1;
+    int left = (pos << 1) | 1;
+    int right = 1 + left;
+    return query(qlow, qhigh, low, mid, left) + query(qlow, qhigh, 1 + mid, high, right);
   }
 
-  int sumQuery( int qlow , int qhigh , vi &array )
-  {
-    return this -> sumRange( qlow , qhigh , 0 , array.size - 1 , 0 );
+  void update(int low, int high, int pos, int& x, int& val) {
+    if(x >= low && x <= high) {
+      if(low == high) {
+        tree[pos] = val;
+        return;
+      }
+      int mid = (low + high) >> 1;
+      int left = (pos << 1) | 1;
+      int right = 1 + left;
+      update(low, mid, left, x, val);
+      update(1 + mid, high, right, x, val);
+      tree[pos] = tree[left] + tree[right];
+    }
   }
 
-  void updateQuery( int index , vi &array , int cantidad )
-  {
-    this -> update( index , 0 , array.size - 1 , 0 , cantidad );
+  int query(int i, int j) {
+    return query(i, j, 0, tam - 1, 0);
+  }
+
+  void update(int pos, int val) {
+    update(0, tam - 1, 0, pos, val);
   }
 
 };
 
-int main()
-{
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL);
+  char op[10];
   bool first = true;
-  int N , index , cantidad , caso = 0;
-  string instruccion;
-  char op;
-  while( scanf("%d", &N ) , N )
-  {
-    if( !first ) puts("");
-    else  first = false;
-    printf("Case %d:\n", ++caso );
-    vi arreglo( N , 0 );
-    foi( i , 0 , N )  scanf("%d", &arreglo[i] );
-    SegmentTree arbol( arreglo );
-    cin.ignore( 1 , '\n' );
-    while( getline( cin , instruccion ) , instruccion != "END" )
-    {
-      sscanf( instruccion.c_str() , "%c %d %d" , &op , &index , &cantidad );
-      if( op == 'M' )
-        printf("%d\n", arbol.sumQuery( index - 1 , cantidad - 1 , arreglo ) );
-      else
-        arbol.updateQuery( index - 1 , arreglo , cantidad );
+  int n,x,y, c = 0;
+  vector < int > arr;
+  while(cin >> n, n) {
+    if(!first)  cout << '\n';
+    first = false;
+    arr.resize(n);
+    foi(i,0,n)  cin >> arr[i];
+    st tree(arr);
+    cout << "Case " << ++c << ":\n";
+    while(cin >> op, op[0] != 'E') {
+      if(op[0] == 'S') {
+        cin >> x >> y;
+        tree.update(x - 1, y);
+      }
+      else {
+        cin >> x >> y;
+        cout << tree.query(x - 1, y - 1) << '\n';
+      }
     }
   }
   return 0;
